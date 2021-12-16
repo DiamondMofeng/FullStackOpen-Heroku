a88bbeb7c5882afc2601cf1fb43292dcbdb8733b
Mofeng <363034937@qq.com>
2021-12-17 02:11:25 +0800

    3.16 done(looks like)

--- index.js
+++ index.js
@@ -10,6 +10,13 @@ const app = express()
 app.use(express.static('build'))
 app.use(express.json())
 app.use(cors())
+//use morgan
+morgan.token('bodyJson', function (req, res) { return JSON.stringify(req.body) })
+app.use(morgan(':method :url :status :res[content-length] - :response-time ms :bodyJson'))
+
+
+
+
 
 const PORT = process.env.PORT || 3001
 app.listen(PORT, () => {
@@ -17,8 +24,6 @@ app.listen(PORT, () => {
 })
 
 
-morgan.token('bodyJson', function (req, res) { return JSON.stringify(req.body) })
-app.use(morgan(':method :url :status :res[content-length] - :response-time ms :bodyJson'))
 
 
 
@@ -47,56 +52,48 @@ let persons = [
 ]
 
 app.get('/api/info', (request, response) => {
-  Person.find({}).then(result => {
-    let personsLength = result.length
-    let str = 'PhoneBook has info for ' + personsLength + ' people  '
-    response.send(`<p>${str}<br/>${Date()}</p>`)
-  })
+  Person.find({})
+    .then(result => {
+      let personsLength = result.length
+      let str = 'PhoneBook has info for ' + personsLength + ' people  '
+      response.send(`<p>${str}<br/>${Date()}</p>`)
+    })
+    .catch(err => next(err))
 })
 
 
 app.get('/api/persons', (request, response) => {
 
-  Person.find({}).then(result => {
-    // console.log(result)
-    response.json(result)
-  })
+  Person.find({})
+    .then(result => {
+      console.log(result)
+      if (result) {
+        response.json(result)
+      } else {
+        response.status(404).end()
+      }
+    })
+    .catch(err => next(err))
 
-  // if (persons) {
-  //   response.json(persons)
-  // }
-  // else {
-  //   response.status(404).end()
-  // }
+
 })
 
-app.get('/api/persons/:id', (request, response) => {
-  // const id = Number(request.params.id)
+app.get('/api/persons/:id', (request, response, next) => {
   const id = request.params.id
-  //old
-  // const person = persons.find(person => person.id === id)
   const person = Person.findById(id)
     .then(result => {
       // console.log(result)
       if (result) {
         // console.log(result)
         response.json(result)
-      }
-      else {
-        console.log("123")
-        response.json({ error: 'there is no person with this id' })
+      } else {
+        // console.log("123")
+        response.status(404).json({ error: 'there is no person with this id' })
       }
     })
     .catch(error => {
-      console.log(error)
+      next(error)
     })
-  // console.log(person)
-  // if (person) {
-  //   response.json(person)
-  // }
-  // else {
-  //   response.status(404).send('There is no such person').end()
-  // }
 })
 
 app.post('/api/persons', (request, response) => {
@@ -129,7 +126,7 @@ app.post('/api/persons', (request, response) => {
 
 })
 //have not updated
-app.delete('/api/persons/:id', (request, response,next) => {
+app.delete('/api/persons/:id', (request, response, next) => {
   const id = request.params.id
   Person.findByIdAndRemove(id)
     .then(result => {
@@ -140,3 +137,28 @@ app.delete('/api/persons/:id', (request, response,next) => {
 })
 
 
+
+
+
+
+
+//// use errorHandler 必须调用next的放在后面
+const unknownEndpoint = (request, response) => {
+  response.status(404).send({ error: 'unknown endpoint' })
+}
+
+// handler of requests with unknown endpoint
+app.use(unknownEndpoint)
+
+const errorHandler = (error, request, response, next) => {
+  // response.status(404).send({ error: 'unknown endpoint' })
+  if (error.name === 'CastError' && error.kind === 'ObjectId') {
+    return response.status(400).send({ error: 'malformatted id' })
+  } else {
+    return response.status(400).send({ error: 'unknow error' })
+  }
+}
+
+
+// handler of requests with result to errors
+app.use(errorHandler)
